f2facd866dd2c3ed5ee03c7c2dbe9ece4e0b5e2b
Alexander Daniluc <alexedaniluc@gmail.com>
2025-08-11 15:25:52 -0600

    Partial Funcionality to fetchoverviewdata()

---
+++ Scrape.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const players = {
+    sk1: "skies",
+    KOS: "3650",
+    Skelesis: "Folk",
+    Master: "BSU",
+    Prestige: "BSU"
+}
+
+const BASEURL = "https://tracker.gg/valorant/profile/riot/{playername}%23{tag}" 
+
+//data we need: Current rank
+//Ingame profile picutre/banner 
+//Current RR
+
+async function fetchOverviewData() {
+    try {
+
+        for(const player in players){
+            const playerURL = BASEURL.replace("{playername}", player).replace("{tag}", players[player]);
+            console.log(playerURL);
+            //const response = await axios();
+        }
+
+    } catch (error) {
+        console.error('Error fetching team data:', error);
+        return null;
+    }
+}
+
+fetchOverviewData();
